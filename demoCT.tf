744024a3f27758dec961306ec4856ca597c4115a
shagunavid <shagun@avidsecure.io>
2018-12-10 13:09:23 +0530

    shagun

--- demoCT.tf
+++ demoCT.tf
@@ -203,7 +203,7 @@ resource "aws_cloudtrail"  "example" {
 
   is_multi_region_trail = true
 
-  #cloud_watch_logs_group_arn    = "aws:arn::log-group:someLogGroup:"
+  cloud_watch_logs_group_arn    = "aws:arn::log-group:someLogGroup:"
   event_selector {
     read_write_type = "All"
     include_management_events = true
